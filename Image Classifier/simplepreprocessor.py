1b52e2f1f574f3a300553caf328e52197f00dbe0
GDIV <gdavisiv@gmail.com>
2020-06-29 13:13:59 -0400

    Completed creation of resizing script

--- Image Classifier/simplepreprocessor.py
+++ Image Classifier/simplepreprocessor.py
@@ -1,9 +1,14 @@
+#import the needed libraries
 import cv2
 
 class SimplePreprocessor:
-	def __intit__(self, width, height, inter=cv2.INTER_AREA)
+	def __intit__(self, width, height, inter=cv2.INTER_AREA):
 	#stores the target image width, height, interpolation
 
 	self.width = width
 	self.height = height
 	self.inter = inter
+
+	def preprocess(self, image):
+		#resize the image for a fixed resolution, ignore aspect ratio
+		return cv2.resize(images, (self.width, self.height), interpolation=self.inter)
