3b0db9e3332243ad661c3dd9f09c00d5be526da5
GDIV <gdavisiv@gmail.com>
2020-06-29 13:03:49 -0400

    Creating simple preprecessor to resize and standardize all images

---
+++ Image Classifier/simplepreprocessor.py
@@ -0,0 +1,9 @@
+import cv2
+
+class SimplePreprocessor:
+	def __intit__(self, width, height, inter=cv2.INTER_AREA)
+	#stores the target image width, height, interpolation
+
+	self.width = width
+	self.height = height
+	self.inter = inter
