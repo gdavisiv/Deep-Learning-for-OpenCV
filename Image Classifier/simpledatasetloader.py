33e7061246927a0e98aa8a7d422c064becdccfb5
GDIV <gdavisiv@gmail.com>
2020-06-30 13:30:06 -0400

    Added commenting and Initialized list of features/labels

--- Image Classifier/simpledatasetloader.py
+++ Image Classifier/simpledatasetloader.py
@@ -3,8 +3,16 @@ import cv2
 import os
 
 class SimpleDatasetLoader:
-	def __intit__(drlg, preprocessors=None):
+	def __intit__(drlg, preprocessors=None):\
+		#Stores the image preprocessor
 		self.preprocessors = preprocessors
 
+		#If preprocessor is none, initialize as an empty list
 		if self.preprocessors is None:
 			self.preprocessors = []
+
+	def load(self, imagePaths, verbose=-1):
+		#Enable the list of features/labels
+		data = []
+		lables = []
+
