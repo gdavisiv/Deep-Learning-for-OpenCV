5d0b0b12309e4d84d7a3b8f4723132b52dd3bfd8
GDIV <gdavisiv@gmail.com>
2020-07-06 15:47:45 -0400

    Completed building Image processor - adding code comments next

--- Image Classifier/simpledatasetloader.py
+++ Image Classifier/simpledatasetloader.py
@@ -28,3 +28,8 @@ class SimpleDatasetLoader:
 
 			data.append(image)
 			labels.append(label)
+
+			if verbose > 0 and i > 0 and (i+1) % verbose == 0:
+				print("[INFO] processed {}/{}".format(i + 1, len(immagePaths)))
+
+		return (np.array(data), np.array(labels))
