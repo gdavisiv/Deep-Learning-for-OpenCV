263e597f6e0b1a5d4f0b831428b1d7c64ee355d3
GDIV <gdavisiv@gmail.com>
2020-07-13 23:53:21 -0400

    Updating knn.py with construction of arguement parse and parsing arguments

--- Image Classifier/knn.py
+++ Image Classifier/knn.py
@@ -12,3 +12,9 @@ from pyimagesearch.datasets import SimpleDatasetLoader
 from imutils import paths
 import argparse
 
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True,
+	help="path to input dataset")
+ap.add_argument("-k", "--neighbors", type=int, default=1, help="# of nearest neighbors for classification")
+ap.add_argument("-j", "--jobs", type=int, default=-1, help="# of jobs for k-NN distance (-1 uses all available cores)")
+args = vars(ap.parse_args())
