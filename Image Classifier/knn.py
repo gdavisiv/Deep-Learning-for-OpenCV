b996a1565738adf115a33aa0bb5dfca294d15d7e
GDIV <gdavisiv@gmail.com>
2020-07-14 23:46:54 -0400

    Completed all code for the image processing, and image data output

--- Image Classifier/knn.py
+++ Image Classifier/knn.py
@@ -18,3 +18,16 @@ ap.add_argument("-d", "--dataset", required=True,
 ap.add_argument("-k", "--neighbors", type=int, default=1, help="# of nearest neighbors for classification")
 ap.add_argument("-j", "--jobs", type=int, default=-1, help="# of jobs for k-NN distance (-1 uses all available cores)")
 args = vars(ap.parse_args())
+
+#get the list of images that will be distributed
+print("[INFO] loading images...")
+imagePaths = lists(paths.list_images(args["dataset"]))
+
+#initialize the image preprocessor, load the dataset, reshape the data matrix
+sp = SimplePreprocessor(32, 32)
+sdl = SimpleDatasetLoader(preprocessors = [sp])
+(data, labels) = sdl.load(imagePaths, verbose=500)
+data = data.reshape((data.shape[0], 3072))
+
+#show info on memory consumption of images
+print("[INFO] fratures matric: {:..1f}MB".format(data.nbytes / (1024 * 1024.0)))
