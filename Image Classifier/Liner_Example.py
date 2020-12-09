d7a893ffb5913ec3a17eee649c2371deca3970ba
GDIV <gdavisiv@gmail.com>
2020-12-08 21:38:31 -0500

    Updated comment code for Linear Example Script

--- Image Classifier/Liner_Example.py
+++ Image Classifier/Liner_Example.py
@@ -15,10 +15,14 @@ b = np.random.randn(3)
 orig = cv2.imread("beagle.png")
 image = cv2.resize(orig, (32, 32)).flatten()
 
+#Computes the output scores via:
+#Taking dot product between the weight matrix W and the input image pixel intensifies
 scores = W.dot(image) + b
 
+#Loops over the scores + labels and prints it to display
 for(label, score) in zip(labels, scores):
 	print("[INFO] {}: {:.2f}".format(label, score))
 
+#draws the label(dog. cat, panda) with the highest scores on the image as our prediction
 cv2.putText(orig, "Label: {}".format(labels[np.argmax(scores)]), 
 	(10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
