38f9c3a48f61eb7a88d17726a089f632f9ecca89
GDIV <gdavisiv@gmail.com>
2020-12-10 23:21:27 -0500

    Updated Gradient Descent Code

--- Image Classifier/gradient_descent.py
+++ Image Classifier/gradient_descent.py
@@ -1,4 +1,13 @@
 #packages needed
-import sklearn.model_selection import train_test_split
-import sklearn.metrics import classification_report
-import sklearn.datasets import make_blobs
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+
+def sigmoid_activation(x):
+	return 1.0 / (1 + np.esp(-x))
+
+def sigmoid_deriv(x):
+	return x * (1-x)
